cfcfe19ec169eeed8a5911c299a1a93eb780647a
treehouseangelo <78700871+treehouseangelo@users.noreply.github.com>
2021-02-08 11:15:44 -0800

    Update script.js

--- Project 1/a_random_quote_generator-v1/js/script.js
+++ Project 1/a_random_quote_generator-v1/js/script.js
@@ -11,7 +11,51 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+// Create an Array of objects to store the data for your quotes
 
+let quotes = [
+  {
+    quote: `A lot of late nights in the gym, a lot of early mornings, especially when your friends are going out, you're going to the gym, 
+    those are the sacrifices that you have to make if you want to be an NBA basketball player.`,
+    source: `Jason Kidd`,
+    citation: `New Jersey Nets`,
+    year: 2007,
+    image: `NULL`
+  },
+  {
+    quote: `A lot of late nights in the gym, a lot of early mornings, especially when your friends are going out, you're going to the gym, 
+    those are the sacrifices that you have to make if you want to be an NBA basketball player.`,
+    source: `Jason Kidd`,
+    citation: `New Jersey Nets`,
+    year: 2007,
+    image: `NULL`
+  },
+  {
+    quote: `Ask not what your teammates can do for you. Ask what you can do for your teammates.`,
+    source: `Magic Johnson`,
+    citation: `Los Angeles Lakers`,
+    year: 1987,
+    image: `NULL`
+  },
+  {
+    quote: `Everybody pulls for David, nobody roots for Goliath.`,
+    source: `Wilt Chamberlain`,
+    citation: `Golden State Warriors`,
+    year: 1965,
+    image: `NULL`
+  },
+  {
+    quote: `If you are afraid of failure you don't deserve to be successful!`,
+    source: `Charles Barkley`,
+    citation: `Phoenix Suns`,
+    year: 1995,
+    image: `NULL`
+  },
+
+
+];
+
+console.log(quotes)
 
 /***
  * `getRandomQuote` function
