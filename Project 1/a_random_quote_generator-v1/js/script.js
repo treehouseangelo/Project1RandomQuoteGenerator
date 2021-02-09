3e2ccf6ec8c11ea8733e00b3f1b7cbe7464d766a
treehouseangelo <78700871+treehouseangelo@users.noreply.github.com>
2021-02-09 12:03:14 -0800

    Update script.js

--- Project 1/a_random_quote_generator-v1/js/script.js
+++ Project 1/a_random_quote_generator-v1/js/script.js
@@ -12,6 +12,7 @@ project 1 - A Random Quote Generator
 ***/
 
 // Create an Array of objects to store the data for your quotes
+// Goal: Store 5 empty objects in the quotes Array. Use 5 properties (quote, source, citation, yeaer, image)
 
 let quotes = [
   {
@@ -55,19 +56,59 @@ let quotes = [
 
 ];
 
-console.log(quotes)
+// Below your array of quote objects, use the console.log() method to log the quotes array to the console. You can click on those objects in the console to open them and inspect their contents.
+
+// console.log(quotes)
 
 /***
  * `getRandomQuote` function
+ * The getRandomQuote function should create a random number, and use that random number to return a random quote object from the quotes array.
 ***/
 
+function getRandomQuote() {
+  var randomNum = Math.floor(Math.random() * quotes.length);  // selects a random quote
+  return quotes[randomNum];  // returns the random quote
+}
 
+console.log(getRandomQuote())
 
 /***
  * `printQuote` function
 ***/
 
+// Create the printQuote function
+// The app should display a new quote each time the user clicks the "Show another quote" button using a printQuote function.
+// You will program the printQuote function to perform three tasks: call the getRandomQuote function, use the returned quote object to build a string of HTML and quote properties, then use that string to display a random quote in the browser.
 
+
+function printQuote(){
+
+
+
+  //Call the random quote.
+  const quote = getRandomQuote(quotes);
+
+  //Use the returned quote object to build a string of HTML and quote properties.
+  //Then use that string to display a random quote in the browser.
+
+  let message = '';
+  if (quote.image) {
+    message += `<img src="${quote.image}" alt="${quote.source}">`;
+  }
+  message += `<p class="quote">${quote.quote}</p>`;
+  message += `<p class="source">${quote.source}`;
+  if (quote.citation) {
+    message += `<span class="citation">${quote.citation}</span>`;
+  }
+  if (quote.year) {
+    message += `<span class="year">${quote.year}</span>`;
+  }
+  message += `</p>`;
+  return [
+    document.querySelector('div').innerHTML = message,
+    document.body.style.backgroundColor = randomRGB(randomValue)
+  ] 
+}
 
 /***
  * click event listener for the print quote button
